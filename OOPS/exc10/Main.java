4b71768dfa00856b411c65423c3ee3914c6f981e
Nitin1610 <118426718+Nitin1610@users.noreply.github.com>
2022-11-22 15:08:13 +0530

    Add files via upload

---
+++ OOPS/exc10/Main.java
@@ -0,0 +1,21 @@
+package com.yash.exc10;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Product product = new Product();
+
+		System.out.println(product.pid + " " + product.pname + " " + product.price + " " + product.unitOfMeasurement);
+
+		Product product1 = product;
+
+		product1.pname = "xyz";
+		product1.price = 300;
+
+		System.out.println(product.pid + " " + product.pname + " " + product.price + " " + product.unitOfMeasurement);
+		System.out
+				.println(product1.pid + " " + product1.pname + " " + product1.price + " " + product1.unitOfMeasurement);
+
+		System.out.println(product1 instanceof Product);
+	}
+}
---
+++ OOPS/exc10/Product.java
@@ -0,0 +1,17 @@
+package com.yash.exc10;
+
+public class Product implements Cloneable {
+
+	int pid;
+	String pname;
+	double price;
+	String unitOfMeasurement;
+
+	public Product() {
+		pid = 1;
+		pname = "rice";
+		price = 200.0;
+		unitOfMeasurement = "1 kg";
+	}
+
+}
