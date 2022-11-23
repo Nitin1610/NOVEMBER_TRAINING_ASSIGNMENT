1b8af8c12824fbb52c783068c6b176133718beca
Nitin1610 <118426718+Nitin1610@users.noreply.github.com>
2022-11-23 15:52:31 +0530

    Add files via upload

---
+++ OOPS/exc3/Main.java
@@ -0,0 +1,13 @@
+package com.yash.exc3;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Customer_Account_Statement customerAccountStatement = new Customer_Account_Statement(1, 6352442, "nitin",
+				"01/01/2021", "01/01/2022", "branch_Obj", 20, 10000.36, 5000.89, "16/11/2022");
+
+		System.out.println(customerAccountStatement);
+
+	}
+}
